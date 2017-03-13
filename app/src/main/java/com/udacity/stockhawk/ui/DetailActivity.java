06115c1247ed973c272275fa29e607bf343ce7d3
Carlos Silva <csilva2810@gmail.com>
2017-03-13 15:22:39 -0300

    Adding some changes to detail view

--- app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -3,6 +3,9 @@ package com.udacity.stockhawk.ui;
 import android.content.Intent;
 import android.database.Cursor;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,6 +17,8 @@ import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
 import com.udacity.stockhawk.model.MyStock;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,9 +28,16 @@ import butterknife.ButterKnife;
 public class DetailActivity extends AppCompatActivity {
 
     private MyStock stock;
+    private DecimalFormat moneyFormat, percentFormat, moneyFormatWithPlus;
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.toolbar_title)
+    TextView toolbarTitle;
+
+    @BindView(R.id.tab_layout)
+    TabLayout tabLayout;
 
     @BindView(R.id.symbol_textview)
     TextView tvSymbol;
@@ -39,8 +51,11 @@ public class DetailActivity extends AppCompatActivity {
     @BindView(R.id.percent_change_textview)
     TextView tvPercentChange;
 
-    @BindView(R.id.history_recyclerview)
-    RecyclerView rvHistory;
+    //@BindView(R.id.history_recyclerview)
+    // RecyclerView rvHistory;
+
+    @BindView(R.id.detail_viewpager)
+    ViewPager vpDetail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,23 +64,60 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         ButterKnife.bind(this);
 
+        initFormatters();
+        initTabs();
+
         Intent extras = getIntent();
         String symbol = extras.getStringExtra(MainActivity.EXTRA_SYMBOL);
 
         stock = getStockForSymbol(symbol);
 
-        toolbar.setElevation(12);
-        toolbar.setTitle(stock.getName());
+        toolbar.setElevation(0);
+        toolbarTitle.setText(stock.getName());
         setSupportActionBar(toolbar);
 
-        tvSymbol.setText(symbol);
-        tvPrice.setText(String.valueOf(stock.getPrice()));
-        tvAbsChange.setText(String.valueOf(stock.getAbsoluteChange()));
-        tvPercentChange.setText(String.valueOf(stock.getPercentageChange()));
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        rvHistory.setLayoutManager(new LinearLayoutManager(this));
-        rvHistory.setNestedScrollingEnabled(false);
-        rvHistory.setAdapter(new HistoryAdapter(this, stock.getHistory()));
+        tvSymbol.setText(symbol);
+        tvPrice.setText( moneyFormat.format(stock.getPrice()) );
+        tvAbsChange.setText( moneyFormatWithPlus.format(stock.getAbsoluteChange()) );
+        tvPercentChange.setText( percentFormat.format(stock.getPercentageChange()) );
+
+        // rvHistory.setLayoutManager(new LinearLayoutManager(this));
+        // rvHistory.setNestedScrollingEnabled(false);
+        // rvHistory.setAdapter(new HistoryAdapter(this, stock.getHistory()));
+
+    }
+
+    protected void initTabs() {
+
+        TabLayout.Tab tabChart = tabLayout.newTab();
+        TabLayout.Tab tabList = tabLayout.newTab();
+
+        tabChart.setIcon(R.drawable.tab_timeline_white).setContentDescription(R.string.tab_chart_content_description);
+        tabList.setIcon(R.drawable.tab_view_list_white).setContentDescription(R.string.tab_list_content_description);
+
+        tabLayout.addTab(tabChart);
+        tabLayout.addTab(tabList);
+
+    }
+
+    protected void initFormatters() {
+
+        moneyFormat = (DecimalFormat) NumberFormat.getCurrencyInstance();
+
+        moneyFormatWithPlus = (DecimalFormat) NumberFormat.getCurrencyInstance();
+        moneyFormatWithPlus.setMaximumFractionDigits(2);
+        moneyFormatWithPlus.setMinimumFractionDigits(2);
+        moneyFormatWithPlus.setNegativePrefix("$-");
+        moneyFormatWithPlus.setPositivePrefix("$");
+
+        percentFormat = (DecimalFormat) NumberFormat.getCurrencyInstance();
+        percentFormat.setMaximumFractionDigits(2);
+        percentFormat.setMinimumFractionDigits(2);
+        percentFormat.setNegativePrefix("%-");
+        percentFormat.setPositivePrefix("%");
 
     }
 
